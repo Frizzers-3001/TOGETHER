1c2b856385321db590e2d76f5b4bb2450c4172ed
guneet-01 <91559944+guneet-01@users.noreply.github.com>
2022-01-22 13:41:37 +0530

    Create doctors_list.php

---
+++ doctors_list.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>Add/Edit Doctors</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Add/Edit Doctors</h2>
+  </div>
+	 
+  <form method="post" action="all.php">
+  	
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="search">Search</button>
+  	</div>
+  	
+  </form>
+</body>
+</html>
